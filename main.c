8db0066b0c0b2f7ace43cbc820926880e194753c
NTELAMO-MUSOMI <225168618@nust.na>
2026-09-11 16:12:44 +0200

    Week 1 -First C Prigram

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char municipality[50];
+
+    printf("Enter Municipality Name: ");
+    fgets(municipality, sizeof(municipality), stdin);
+
+    // Remove the newline character added by fgets()
+    municipality[strcspn(municipality, "\n")] = '\0';
+
+    printf("Municipality: %s\n", municipality);
+
+    return 0;
+}
